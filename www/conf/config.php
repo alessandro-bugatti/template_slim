645fdd4325476b856b402335c6cf8c1f59cf8494
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2026-05-01 10:23:39 +0200

    Aggiorna il file di configurazione per utilizzare le variabili d'ambiente.

--- www/conf/config.php
+++ www/conf/config.php
@@ -1,16 +1,15 @@
 <?php
+/**
+ * File di configurazione che recupera i valori attuali
+ * dal file con le varaibili d'ambiente (.env)
+ */
 
-const DB_HOST = 'localhost';
-const DB_NAME = 'negozio';
-const DB_USER = 'root';
-const DB_PASSWORD = '';
-const DB_CHAR = 'utf8';
-
-//Sottocartella dove viene eseguita l'applicazione
-const BASE_PATH = '/template_slim';
+// Database
+define('DB_HOST', $_ENV['MYSQL_HOST']     ?? 'database');
+define('DB_NAME', $_ENV['MYSQL_DATABASE'] ?? '');
+define('DB_USER', $_ENV['MYSQL_USER']     ?? '');
+define('DB_PASSWORD', $_ENV['MYSQL_PASSWORD'] ?? '');
+define('DB_CHAR', 'utf8mb4');
 
 //Attiva il gestore di errori personalizzato
-const MY_ERROR_HANDLER = true;
-
-//Cartella dove verranno memorizzate le immagini
-const IMAGES = 'images';
+define('MY_ERROR_HANDLER', $_ENV['MYSQL_PASSWORD'] != 'development');
