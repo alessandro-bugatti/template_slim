308ece6e1a7028b53b7a0cf89e424f4876092397
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2026-05-01 10:09:56 +0200

    Aggiunge i file specifici della nuova architettura fatta con Docker.

---
+++ www/conf/config.php
@@ -0,0 +1,16 @@
+<?php
+
+const DB_HOST = 'localhost';
+const DB_NAME = 'negozio';
+const DB_USER = 'root';
+const DB_PASSWORD = '';
+const DB_CHAR = 'utf8';
+
+//Sottocartella dove viene eseguita l'applicazione
+const BASE_PATH = '/template_slim';
+
+//Attiva il gestore di errori personalizzato
+const MY_ERROR_HANDLER = true;
+
+//Cartella dove verranno memorizzate le immagini
+const IMAGES = 'images';
